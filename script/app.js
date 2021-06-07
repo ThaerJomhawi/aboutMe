86bcbf430b2138e1374820019160bd5a61ac6cff
Thaer AlJomhawi <thaer.fjomhawi@gmail.com>
2021-06-07 16:44:50 +0300

    creating about me repo with 5 Qustions about myself with js

---
+++ script/app.js
@@ -0,0 +1,94 @@
+/*'use strict';
+
+let userName=prompt('Welcome to my page please enter your Name');
+alert('Welcome again ('+userName+') to my page I will ask you some questions about myself and you will answer by yes or no');
+
+//First question
+let myName= prompt('My name is Thaer?');
+if(myName.toLowerCase() ==='yes' || myName.toLowerCase() ==='y')
+  {
+    alert('well done. yes my name is Thaer');
+  console.log('true');
+  
+} 
+ else if(myName.toLowerCase() ==='no' || myName.toLowerCase() ==='n')
+{
+  alert('sorry . accully its Thaer , now you know my name');
+  
+}
+ else{ alert(' you should answer by yes or no'); }
+ 
+ 
+ //Second question
+
+let unMaj= prompt('Do you think I studied software development in the university ?');
+
+if(unMaj.toLowerCase() ==='yes' || unMaj.toLowerCase() ==='y')
+  {
+  alert('you are wrong , I studied Chemical Engineering');
+  
+} 
+else if(unMaj.toLowerCase() ==='no' || unMaj.toLowerCase() ==='n')
+{
+  alert('thats correct , I studied Chemical Engineering');
+  console.log(' true');
+  
+}
+else{ alert(' you should answer by yes or no');}
+
+//Third question
+ 
+let favMeal= prompt('My favorite meal is Mansaf?');
+
+if(favMeal.toLowerCase() ==='yes' || favMeal.toLowerCase() ==='y')
+{
+    alert('yes this is true  ');
+    console.log('true');
+    
+  } 
+
+  else if(favMeal.toLowerCase() ==='no' || favMeal.toLowerCase() ==='n')
+  { alert('thats wrong , from east to west mansaf is the best');  }
+
+  else{  alert('you should answer by yes or no');  }
+
+
+// forth question
+
+  let favColor= prompt('My favorite color is Grey?');
+  if(favColor.toLowerCase() ==='yes' || favColor.toLowerCase() ==='y')
+  {
+    alert('thats correct');
+    console.log(' true');
+    
+  } else if(favColor.toLowerCase() ==='no' || favColor.toLowerCase() ==='n')
+  {
+    alert('thats wrong');
+    
+  }
+else{
+  alert('sorry you should answer by yes or no');
+}
+
+
+//Fifth question
+
+  let favSeries= prompt('Is Game of Thrones my favorite Series?');
+  if(favSeries.toLowerCase() ==='yes' || favSeries.toLowerCase() ==='y')
+  {
+    alert('thats correct, you should watch it ');
+    console.log('true');
+    
+} else if(favSeries.toLowerCase() ==='no' || favSeries.toLowerCase() ==='n')
+{
+  alert('thats wrong , its is my favorite ');
+  
+}
+else{
+  alert('you should answer by yes or no');
+  
+}
+
+
+alert(`Thank you so much ${userName} for being my guest , hope you enjoyed. please continue and read the introduction about me in the webpage`);
+*/
